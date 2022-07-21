7136675b97fe9a6ff67ce75f1192afa5977f6cff
Samuel Newhouse <samuel_newhouse@yahoo.com>
2022-07-21 16:34:39 -0500

    Add BGParticles component to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 import Rank from "./components/rank/Rank";
+import BGParticles from "./components/bgParticles/BGParticles";
 
 function App() {
   return (
     <div className="App">
-			<Navigation />
-			<Logo />
+      <BGParticles />
+
+      <Navigation />
+      <Logo />
       <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
