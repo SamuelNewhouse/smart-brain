017f2fd58967dbf8c197f7e1f8460a26699a08ee
Samuel Newhouse <samuel_newhouse@yahoo.com>
2022-07-21 16:29:24 -0500

    Reformat Logo component code

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,15 +1,17 @@
-import Tilt from 'react-tilt';
-import './logo.css';
-import brain from './brain-logo.png';
+import Tilt from "react-parallax-tilt";
+import "./logo.css";
+import brain from "./brain-logo.png";
 
 const Logo = () => {
-	return (
-		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-				<div className="Tilt-inner flex h-100"><img alt='logo' src={brain}/></div>
-			</Tilt>
-		</div>
-	);
-}
+  return (
+    <div className="ma4 mt0">
+      <Tilt className="Tilt br2 shadow-2" tiltReverse={true} style={{ height: 150, width: 150 }}>
+        <div className="Tilt-inner flex h-100">
+          <img alt="logo" src={brain} />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
 
 export default Logo;
