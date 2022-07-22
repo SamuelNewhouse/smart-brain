2493d9b27ca09ecf51edab0d609c90c21d1602df
Samuel Newhouse <samuel_newhouse@yahoo.com>
2022-07-21 19:12:50 -0500

    Add state and input handlers

--- src/App.js
+++ src/App.js
@@ -4,19 +4,40 @@ import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 import Rank from "./components/rank/Rank";
 import BGParticles from "./components/bgParticles/BGParticles";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <BGParticles />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
 
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <BGParticles />
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,12 +1,19 @@
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
-      <p className="f3">This Magic Brain will detect faces in your pictures. Give it a try.</p>
+      <p className="f3">
+        This Magic Brain will detect faces in your pictures. Give it a try.
+      </p>
       <div className="flex w-70 center form pa4 br3">
-        <input type="text" className="f4 pa2 w-70" />
-        <button className="w-30 grow f4 dib link ph3 pv2 white bg-light-purple">Detect</button>
+        <input type="text" className="f4 pa2 w-70" onChange={onInputChange} />
+        <button
+          className="w-30 grow f4 dib link ph3 pv2 white bg-light-purple"
+          onClick={onButtonSubmit}
+        >
+          Detect
+        </button>
       </div>
     </div>
   );
