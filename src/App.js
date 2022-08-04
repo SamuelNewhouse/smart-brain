d52bf69a26f23b132ff5b43b0d47e762a683a0a9
Samuel Newhouse <samuel_newhouse@yahoo.com>
2022-08-04 14:20:46 -0500

    Add FaceRecognition component to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import "./App.css";
-import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
+import Navigation from "./components/navigation/Navigation";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 import Rank from "./components/rank/Rank";
+import Clarifai from "clarifai";
 import BGParticles from "./components/bgParticles/BGParticles";
 import { Component } from "react";
+
+const app = new Clarifai.App({
+  apiKey: "a73d20da20bf435d9de9a1d60e205db6",
+});
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: "",
+      imageUrl: "",
+      box: {},
     };
   }
 
+  // 2:53 in Face Detection Box
+	// TEST URL: https://cdn.bleacherreport.net/images/team_logos/328x328/the_rock.png
+  calculateFaceLocation = (data) => {
+    const faceData = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: faceData.left_col * width,
+      topRow: faceData.top_row * height,
+      rightCol: width - faceData.right_col * width,
+      bottomRow: height - faceData.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box });
+  };
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
-    console.log("click");
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => this.calculateFaceLocation(response))
+      .then((result) => this.displayFaceBox(result))
+      .catch((err) => console.log(err));
   };
 
   render() {
@@ -34,7 +68,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* <FaceRecognition /> */}
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
